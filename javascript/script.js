32ff93723564d63c45210334197dfa43d0f60c02
Sasha Ivanova <aliaksandra.v.ivanova@gmail.com>
2020-04-12 22:15:28 +0200

    html is fixed

--- javascript/script.js
+++ javascript/script.js
@@ -9,7 +9,7 @@ const popupName = document.querySelector('.popup__text_type_name');
 const popupProfession = document.querySelector('.popup__text_type_profession');
 const popupForm = document.querySelector('.popup__container');
 
-
+//функция открытия формы по нажатию на кнопку Edit
 function onEditButtonClicked(event) {
     event.preventDefault();
     popup.classList.add('popup_opened');
@@ -17,11 +17,13 @@ function onEditButtonClicked(event) {
     popupProfession.value = profileProfession.textContent;
 }
 
+//функция закрытия формы по нажатию на кнопку Close, изменения не сохраняются
 function onCloseButtonClicked(event) {
     event.preventDefault();
     popup.classList.remove('popup_opened');
 }
 
+//функция закрытия формы и сохранения внесенных изменения по нажатию на кнопку Submit
 function onSubmitForm(event) {
     event.preventDefault();
     profileName.textContent = popupName.value; 
