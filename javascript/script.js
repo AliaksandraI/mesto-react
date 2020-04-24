1d52b373612672f4bfb381f42bd95e78f8da22e5
Sasha Ivanova <aliaksandra.v.ivanova@gmail.com>
2020-04-24 22:21:52 +0200

    template of card draft

--- javascript/script.js
+++ javascript/script.js
@@ -37,3 +37,56 @@ editButton.addEventListener('click', toggleForm);
 closeButton.addEventListener('click', toggleForm);
 
 popupForm.addEventListener('submit', onSubmitForm);
+
+//array
+
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+//template
+function addCard(link, name) {
+
+const cardTemplate = document.querySelector('#card').content;
+const cardAdded = document.querySelector('.elements__item');
+
+const cardElement = cardTemplate.cloneNode(true);
+
+cardElement.querySelector('.elements__item-picture').src = link;
+cardElement.querySelector('.elements__item-title').textContent = name;
+
+cardAdded.append(cardElement);
+}
+
+/*  addButton.addEventListener('click', function () {
+    const     = document.querySelector('.input__text_type_link');
+    const     = document.querySelector('.input__text_type_name');
+  
+    addCard(link.value, name.value);
+  
+    artist.value = '';
+    title.value = '';
+  });
