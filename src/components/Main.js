62ab28ade48a6cd807d6e6479bea2932f907c287
Sasha Ivanova <aliaksandra.v.ivanova@gmail.com>
2020-07-21 23:14:28 +0200

    api is done

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import api from '../utils/Api';
+
 import penPath from '../images/pen.svg';
 import notFoundImagePath from '../images/not_found.svg';
 import editButtonPath from '../images/edit__button.svg';
@@ -12,8 +14,32 @@ class Main extends React.Component {
   
     constructor(props) {
         super(props);
+
+        this.state = {
+            userName: null,
+            userDescription: null,
+            userAvatar: null
+        }
     }
 
+    componentDidMount() {
+        api.getUserInfo()
+        .then (user => {
+            this.setState({ userName: user.name,
+                            userDescription: user.about,
+                            userAvatar: user.avatar
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        }); 
+
+    }
+
+    onImageNotFound = (evt) => {
+        evt.target.onerror = null;
+        evt.target.src = notFoundImagePath;
+    }
 
     render () {
 
@@ -23,20 +49,21 @@ class Main extends React.Component {
                     <div className="profile__info">
                         <div className="profile__avatar">
                             <img src={penPath} className="profile_active" alt=" Изменение аватарки"></img>
-                            <img onError={() => { this.onerror = null; this.src = notFoundImagePath; }} 
-                            onClick={this.props.onEditAvatar} 
-                            className="profile__image" alt="Аватар"></img>
+                            <img src={this.state.userAvatar} 
+                                onError={this.onImageNotFound} 
+                                onClick={this.props.onEditAvatar} 
+                                className="profile__image" alt="Аватар"></img>
                         </div>
                         <div>
                             <div className="profile__name-container">
-                                <h1 className="profile__name"></h1>
+                                <h1 className="profile__name">{this.state.userName}</h1>
                                 <button aria-label="edit" type="button" 
                                 onClick={this.props.onEditProfile} 
                                 className="profile__edit-button">
                                     <img src={editButtonPath} alt="Кнопка изменения"></img>
                                 </button>
                             </div>
-                            <p className="profile__profession"></p>
+                                <p className="profile__profession">{this.state.userDescription}</p>
                         </div>
                     </div>
                     <button aria-label="add" type="button"
@@ -51,7 +78,7 @@ class Main extends React.Component {
 
                 <template id="card-template">
                     <div className="elements__item">
-                        <img onError={() => { this.onerror = null; this.src = notFoundImagePath; }} className="elements__item-picture" alt="Картинка"></img>
+                        <img onError={this.onImageNotFound} className="elements__item-picture" alt="Картинка"></img>
                         <div className="elements__item-info">
                             <h2 className="elements__item-title"></h2>
                             <div className="elements__likes-container">
